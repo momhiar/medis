ee544d36bd88bbe7560e510ea43492c059cb5549
Mohammad Esmaeili <momhiar@gmail.com>
2024-02-05 19:14:58 +0330

    update command libs

--- app/Commands/commands_lib.py
+++ app/Commands/commands_lib.py
@@ -2,7 +2,10 @@ from . import basic_commands
 
 commands_list = {
     'GET': basic_commands.GetCommand,
+    'SET': basic_commands.SetCommand,
     'DEL': basic_commands.DeleteCommand,
     'PING': basic_commands.PingCommand,
-    'TTL': basic_commands.TTLCommand,    
+    'TTL': basic_commands.TTLCommand,
+    'COMMAND': basic_commands.StartCommand,
+    'WRONG': basic_commands.ErrorCommand,    
 }
