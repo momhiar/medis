d38acd7372f2f9ec2f8f17ee8c7d84facc933421
Mohammad Esmaeili <momhiar@gmail.com>
2024-02-05 14:44:07 +0330

    add: persistor commands

--- app/Store/tests/test_persistors.py
+++ app/Store/tests/test_persistors.py
@@ -1,1 +1,15 @@
-#TODO: add persistors tests
+#TODO: add more persistors tests
+import os
+from Store.persistors import PicklePersistor
+
+def test_pickle_persistor_write_data():
+    file = 'db_persist_test.pickle'
+    data = {'record_1': ('test', 1707130406200), 'record_2': ('test2', None)}
+    res = PicklePersistor().sync_latest_to_file(file=file, data=data)
+    assert os.path.exists(file)
+
+def test_pick_persistor_read_data():
+    file = 'db_persist_test.pickle'
+    res = PicklePersistor().read_latest_from_file(file=file)
+    assert res is not None
+    assert res.get('record_2') == ('test2', None)
