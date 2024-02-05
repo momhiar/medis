368c38c1fb3e5e4e5963aa0efd6e223b9eeb5350
Mohammad Esmaeili <momhiar@gmail.com>
2024-02-05 14:25:03 +0330

    fix db persis file errors

--- app/Store/db.py
+++ app/Store/db.py
@@ -1,6 +1,12 @@
 from threading import Lock
 import time
 import pickle
+from .persistors import Persistor, PicklePersistor
+import warnings
+
+
+
+# using singleton patter ( Thread-safe)
 class SingletonInMemStoreMeta(type):
     _instances = {}
     _lock: Lock = Lock()
@@ -10,17 +16,8 @@ class SingletonInMemStoreMeta(type):
         Possible changes to the value of the `__init__` argument do not affect
         the returned instance.
         """
-        # Now, imagine that the program has just been launched. Since there's no
-        # Singleton instance yet, multiple threads can simultaneously pass the
-        # previous conditional and reach this point almost at the same time. The
-        # first of them will acquire lock and will proceed further, while the
-        # rest will wait here.
+
         with cls._lock:
-            # The first thread to acquire the lock, reaches this conditional,
-            # goes inside and creates the Singleton instance. Once it leaves the
-            # lock block, a thread that might have been waiting for the lock
-            # release may then enter this section. But since the Singleton field
-            # is already initialized, the thread won't create a new object.
             if cls not in cls._instances:
                 instance = super().__call__(*args, **kwargs)
                 cls._instances[cls] = instance
@@ -30,8 +27,16 @@ class SingletonInMemStoreMeta(type):
 class SingletonInMemStore(metaclass=SingletonInMemStoreMeta):
     __objects = {}
     
+    #implementation of observer patterns (mono)
+    # TODO: we should add dynamic multiple choices for users at runtime
+    #currently we skip
+    __persistor : Persistor = PicklePersistor()
+    #TODO: we should add logic for dynamic persistor type
+    # for now we skip because lack of time
+    __persist_file = 'db_persist.pickle'
     def set_value_to_objects(self, key, value, expiry_date: int = None):
         self.__objects[key] = (value, expiry_date)
+        self.sync_persistors_from_latest()
         return 'OK'
 
     def get_value_from_objects(self, key):
@@ -49,6 +54,7 @@ class SingletonInMemStore(metaclass=SingletonInMemStoreMeta):
     def delete_record_from_objects(self, key):
         if self.__objects.get(key):
             del self.__objects[key]
+            self.sync_persistors_from_latest()
             return 'OK'
         
     def get_ttl_from_objects(self, key):
@@ -58,3 +64,15 @@ class SingletonInMemStore(metaclass=SingletonInMemStoreMeta):
         if record[1] is None:
             return -1 
         return record[1]
+       
+    #implementation of observer patterns but only one observer
+    def sync_persistors_from_latest(self):
+            self.__persistor.sync_latest_to_file(self.__objects, self.__persist_file)
+    
+    def sync_data_from_persistor(self):
+        try:
+            self.__objects = self.__persistor.read_latest_from_file(self.__persist_file)
+        except FileNotFoundError:
+            warnings.warn('no persis file is there')
+        except:
+            raise EOFError('Error: somthing happened during read persist files')
